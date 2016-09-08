745d027d4ccbd11896ab56c2570cffaad488b157
Kabir Khan <kkhan@redhat.com>
2016-09-08 16:30:05 +0100

    Add common, data, services and rest sample data files

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,12 @@ import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./components/app/app.component";
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  declarations: [
-    AppComponent
-  ],
-  bootstrap: [ AppComponent ]
+    imports: [
+        BrowserModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
